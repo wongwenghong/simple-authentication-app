839f4304320233a9474c7fdb07d0b8b6f7ba64e7
wongwenghong <wong.wh217@gmail.com>
2024-04-11 18:51:07 +0800

    change build to dist in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,11 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
     )
   );
 } else {
