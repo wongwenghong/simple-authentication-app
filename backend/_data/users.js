11d862bd613c81d2d1c2d0936a43977c6fa66425
wongwenghong <wong.wh217@gmail.com>
2024-04-13 10:04:18 +0800

    modified _data/users.js email from gmail to email.com

--- backend/_data/users.js
+++ backend/_data/users.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 const users = [
   {
     name: "Admin",
-    email: "admin@gmail.com",
+    email: "admin@email.com",
     password: bcrypt.hashSync("123456", 10),
   },
 ];
